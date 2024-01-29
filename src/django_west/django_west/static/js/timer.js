1119fffd68fc74865e0158cffb46e7f9dd696084
Marc Rotsaert <marcrotsaert@hotmail.com>
2024-01-29 11:56:51 +0100

    script to timer.js

---
+++ src/django_west/django_west/static/js/timer.js
@@ -0,0 +1,6 @@
+function show_spinner() {
+    // console.log('gg');
+    var spin = document.getElementById("spinnerContainer");
+    spin.classList.remove("d-none");
+    // console.log('lalalala');
+  }
