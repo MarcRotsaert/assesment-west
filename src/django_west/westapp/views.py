8004de977ca5b8633a1a71b437e5a961ef895b03
Marc Rotsaert <marcrotsaert@hotmail.com>
2024-01-29 09:08:13 +0100

    basic django action

--- src/django_west/westapp/views.py
+++ src/django_west/westapp/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def lowest(request):
     if request.method=="GET":
+        # print(request.body)
+        print(dir(request))
+        num = request.GET.get("number", None)
+        print(num)
+        print(request.content_params)
         print("lowest")
         return render(request, "base.html")
+
