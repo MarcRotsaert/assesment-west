ac8c9c08d77120ab003c85e68b10fb6bcb08f267
Marc Rotsaert <marcrotsaert@hotmail.com>
2024-01-30 19:42:52 +0100

    unittest return_lowest

--- src/django_west/westapp/tests.py
+++ src/django_west/westapp/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
-
+import westapp.views as views
 # Create your tests here.
+
+class ReturnLowest(TestCase):
+    # def setUp(self):
+    
+    def test_return_lowest1(self):
+        number = 10
+        result = views.return_lowest(number)
+        self.assertEqual(result, 2520)
+
+    def test_return_lowest2(self):
+        number = 25
+        result = views.return_lowest(number)
+        self.assertEqual(result, 26771144400)
+
+    def test_return_lowest3(self):
+        number = 25
+        result = views.return_lowest(number, 5)
+        self.assertEqual(result, "Takes too long")
