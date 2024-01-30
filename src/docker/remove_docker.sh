e42936530ef1f181f557f2097cb7b9dbc2cb0a7c
marcrotsaert@yahoo.com <marcrotsaert@yahoo.com>
2024-01-30 11:56:44 +0100

    clean up

--- src/docker/remove_docker.sh
+++
@@ -1,7 +0,0 @@
-# remove west docker
-container_name=westassesment
-image_name=westassesment
-# docker ps -f name=kean_mclaren
-# docker kill "$(docker container ls -f name=${container_name} -q)"
-docker images -f reference=${image_name}
-docker rmi "$(docker images -f reference=${image_name} -q)"
--- start_lowest.sh
+++
@@ -1,8 +0,0 @@
-container="westassesment"
-version="0.1"
-west_image="${container}:${version}"
-echo "${west_image}"
-
-docker run --rm -idt -p 8080:8080 --name "${container}" "${west_image}"
-sleep 10
-python -m webbrowser -t "http://127.0.0.1:8080/algoritm"
